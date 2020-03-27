e35e834bfe4f03f270a296cc1a3cc8d19a0084c8
Emre Ceyhan <emreceyhan98@hotmail.com>
2020-03-27 21:14:41 +0000

    Bottom navigation bar commented out

--- app/FoodBank/TempName/App.js
+++ app/FoodBank/TempName/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+//const Tab = createBottomTabNavigator();
+
+/*function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}*/
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>test</Text>
     </View>
+    
   );
 }
 
