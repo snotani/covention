90ad0ed84fe8253b4aef31fb790efacfd6e4a8f4
shailshah26 <shailshah26@yahoo.co.uk>
2020-03-29 17:23:31 +0100

    Added marker to map

--- app/FoodBank/TempName/screens/MapScreen.js
+++ app/FoodBank/TempName/screens/MapScreen.js
@@ -1,55 +1,76 @@
-import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-
-export default class App extends React.Component {
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 0;
+const LONGITUDE = 0;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+    };
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      },
+    (error) => console.log(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      }
+    );
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <MapView 
-            provider = {PROVIDER_GOOGLE}
-            style={styles.mapStyle} 
-            region={{
-                latitude: 54.0039,
-                longitude: -2.7890,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-            }}
-            showsUserLocation
+      <MapView
+        provider={ PROVIDER_GOOGLE }
+        style={ styles.container }
+        showsUserLocation={ true }
+        region={ this.state.region }
+        onRegionChange={ region => this.setState({region}) }
+        onRegionChangeComplete={ region => this.setState({region}) }
+      >
+        <MapView.Marker
+          coordinate={ this.state.region }
         />
-      </View>
+      </MapView>
     );
   }
 }
-
-// findCurrentLocation =() => {
-//   navigator.geolocation.getCurrentPosition(
-//     position => {
-//       const latitude =JSON.stringify(position.coords.latitude);
-//       const longitude = JSON.stringify(position.coords.longitude);
-
-//       this.setState({
-//         latitude,
-//         longitude
-//       });
-//     },
-//     {
-//       enableHighAccuracy: true, timeout:20000, maximumAge: 1000
-//     }
-//   )
-// }
-
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
+    height: '100%',
+    width: '100%',
+  }
 });
-
+AppRegistry.registerComponent('MapExample', () => MapExample);
